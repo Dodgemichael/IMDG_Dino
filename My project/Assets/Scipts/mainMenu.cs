f78634cecd84640a08338f16a48064571554d95a
Dodgemichael <144144427+Dodgemichael@users.noreply.github.com>
2023-09-20 17:45:29 -0400

    Menu Fix

--- My project/Assets/Scipts/mainMenu.cs
+++ My project/Assets/Scipts/mainMenu.cs
@@ -7,5 +7,16 @@ public class mainMenu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+    public void Credits(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+    }
+    public void PatchNotes(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +3);
+    }
+    public void FirstGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +4);
+    }
+
+
 
 }
