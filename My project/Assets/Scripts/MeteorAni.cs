1616a92c36048fb2a10f807ad9ec88f4e3dce129
Dodgemichael <144144427+Dodgemichael@users.noreply.github.com>
2023-09-27 18:10:03 -0400

    Second Spawner

--- My project/Assets/Scripts/MeteorAni.cs
+++ My project/Assets/Scripts/MeteorAni.cs
@@ -1,7 +1,32 @@
-
 using UnityEngine;
+public class MeteorAni: MonoBehaviour{
 
-public class MeteorAni : MonoBehaviour
-{
-   
+    public Sprite[] sprites;
+
+    private SpriteRenderer spriteRenderer;
+    private int frame;
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable(){
+        Invoke(nameof(AnimateM),0f);
+    }
+    
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+    private void AnimateM()
+    {
+        frame++;
+        if(frame >= sprites.Length){
+            frame = 0;
+        }
+
+        if (frame>= 0 && frame < sprites.Length){
+            spriteRenderer.sprite = sprites[frame];
+        }
+
+        Invoke(nameof(AnimateM), 2f / GameManager.Instance.gameSpeed);
+    }
 }
